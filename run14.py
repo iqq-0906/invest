d5f4a4aa70436a84d8906decc3ea95b922c27d58
iqq-0906 <camellia09030713@gmail.com>
2024-09-04 16:52:23 +0800

    Update run14.py

--- run14.py
+++ run14.py
@@ -152,12 +152,12 @@ for l in range(1, 6):
                 cov_data=pd.read_csv('data/BL_cov_rolling{}_{}_{}.csv'.format(l,i,j))
 
                 del cov_data['Unnamed: 0']
-                covariance_matrix = torch.tensor(cov_data.values).float()
-                ones = torch.ones(i)
+                covariance_matrix = torch.tensor(cov_data.values).float().to(device)
+                ones = torch.ones(i).to(device)
                 tensor_list = []
                 for m in range(10):
                     numpy_array = pre_data_return.iloc[:, m].to_numpy(dtype=np.float64)
-                    tensor_name = torch.tensor(numpy_array, dtype=torch.float64).reshape(-1, ).float()
+                    tensor_name = torch.tensor(numpy_array, dtype=torch.float64).reshape(-1, ).float().to(device)
                     torch.set_printoptions(precision=6)
                     tensor_list.append(tensor_name)
                 kan  = KAN(width=[4,2,1,1], grid=3, k=3, seed=1, device=device)

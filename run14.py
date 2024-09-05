c849fc987f8607b5fd0d34c7031953dc0bbffcb0
iqq-0906 <camellia09030713@gmail.com>
2024-09-05 18:34:56 +0800

    Update run14.py

--- run14.py
+++ run14.py
@@ -123,7 +123,7 @@ class r_th(nn.Module):
             #     # 跳过当前 epoch，重新训练
 
             # 合并损失函数，权重根据实际情况调整
-            loss =loss1+loss3+loss5
+            loss =100*loss1+loss3+loss5
 
             # 反向传播和优化
             self.optimizer.step()
